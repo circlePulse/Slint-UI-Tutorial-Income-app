7ae27b1542b196a0445fab38961c1a69123baa9c
omarhboubati <omarhbo23@gmail.com>
2024-05-15 13:49:55 -0700

    Finishing UI

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
 slint::include_modules!();
 
+const TAXPER: f64 = 0.3;
+const OWNERPER: f64 = 0.55;
+const PROFITPER: f64 = 0.05;
+const OPEXPER: f64 = 0.10;
+
 fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
+    let ui_handle = ui.as_weak();
 
-    /*ui.on_request_increase_value({
-        let ui_handle = ui.as_weak();
+    ui.on_divide_income({
         move || {
             let ui = ui_handle.unwrap();
             ui.set_counter(ui.get_counter() + 1);
         }
-    });*/
+    });
 
     ui.run()
 }
