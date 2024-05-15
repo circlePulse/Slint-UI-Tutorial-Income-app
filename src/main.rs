64f4af633b0ab24c02ea8343141b1861b6e6d51d
omarhboubati <omarhbo23@gmail.com>
2024-05-15 14:02:27 -0700

    Business logic

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,18 @@ fn main() -> Result<(), slint::PlatformError> {
     let ui_handle = ui.as_weak();
 
     ui.on_divide_income({
-        move || {
+        move |string| {
             let ui = ui_handle.unwrap();
-            ui.set_counter(ui.get_counter() + 1);
+            let num: f64 = string.trim().parse::<f64>().unwrap();
+
+            let tax: f64 = num * TAXPER;
+            let owner: f64 = num * OWNERPER;
+            let profit: f64 = num * PROFITPER;
+            let opex: f64 = num * OPEXPER;
+
+            let result: String = format!("Tax: {:.2}\nOwner: {:.2}\nProfit: {:.2}\nOpex: {:.2}",
+                                         tax, owner, profit, opex);
+            ui.set_results(result.into());
         }
     });
 
